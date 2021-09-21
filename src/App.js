894340a73be0e29562ba4a70753e8504bb5c857e
Wolf Niklas <niklas.schellhoeh@gmail.com>
2021-09-21 21:43:50 +0200

    commit readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import CurrentConditions from "./CurrentConditions";
+
+const baseUrl = process.env.REACT_APP_BASE_URL;
 
 function App() {
+  const [latestWeatherDataRecord, setLatestWeatherDataRecord] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(baseUrl)
+      .then((response) => {
+        setLatestWeatherDataRecord(response.data);
+      })
+      .catch(function (error) {
+        if (error.response) {
+          console.log('error.response.status', error.response.status);
+          console.log('error.response.headers', error.response.headers);
+        } else if (error.request) {
+          console.log('error.request', error.request);
+        } else {
+          console.log("Error", error.message);
+        }
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-auto md:max-w-md">
+      {latestWeatherDataRecord.length > 0 && (
+        <CurrentConditions data={latestWeatherDataRecord[0]} />
+      )}
+      {/* {latestWeatherDataRecord.map((item, index) => {
+        const date = new Date(item.created_at).toLocaleDateString("de-DE");
+        const time = new Date(item.created_at).toLocaleTimeString("de-DE");
+        return (
+          <div key={index}>
+            <h2>
+              {date}, {time}
+            </h2>
+            <h3>Temperature: {item.temp}</h3>
+            <h3>Humidity: {item.hum}</h3>
+          </div>
+        );
+      })} */}
     </div>
   );
 }
---
+++ src/CurrentConditions.js
@@ -0,0 +1,29 @@
+import { WiThermometer, WiHumidity } from "react-icons/wi";
+
+function CurrentConditions(props) {
+  return (
+    <div className="m-5">
+      <div className="rounded-xl shadow-2xl text-lg font-bold text-blue-500 font-mono">
+        <h2 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 rounded-tr-xl rounded-tl-xl text-white p-4 py-3 uppercase font-bold">
+          Current Conditions
+        </h2>
+        <h3 className="p-2 pr-4 border-b-2 border-opacity-50 flex flex-row items-center">
+          <WiThermometer className="w-10 h-10 inline mr-2" />
+          {props.data.temp}°C
+          <span className="uppercase text-gray-100 ml-auto">Temperature</span>
+        </h3>
+        <h3 className="p-2 pr-4 border-b-2 border-opacity-0 rounded-br-xl rounded-bl-xl flex flex-row items-center">
+          <WiHumidity className="w-10 h-10 inline mr-2" />
+          {props.data.hum}%
+          <span className="uppercase text-gray-100 ml-auto">Humidity</span>
+        </h3>
+      </div>
+      <h3 className="text-xs mt-3 text-right text-gray-400">
+        fetched: {new Date(props.data.created_at).toLocaleDateString("en-US")},{" "}
+        {new Date(props.data.created_at).toLocaleTimeString("de-DE")}
+      </h3>
+    </div>
+  );
+}
+
+export default CurrentConditions;
